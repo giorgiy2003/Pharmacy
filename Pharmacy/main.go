07c16e7240a1d63566d5e44a59f273a966043005
Giorgiy Baroyan <giorgiy_03@mail.ru>
2022-11-23 19:02:56 +0300

    correcive

--- Pharmacy/main.go
+++ Pharmacy/main.go
@@ -9,7 +9,6 @@ import (
 func main() {
 	router := gin.Default()
 	router.Static("/css", "Frontend/css")
-	router.Static("/scss", "Frontend/scss")
 	router.Static("/fonts", "Frontend/fonts")
 	router.Static("/images", "Frontend/images")
 	router.Static("/js", "Frontend/js")
@@ -17,11 +16,11 @@ func main() {
 	router.Use(Handler.ConnectDB())
 	router.GET("/", Handler.MainForm) //Главная страница ✅
 	router.GET("/shop", Handler.Shop) //Магазин ✅
-	router.GET("/NameASC", Handler.NameASC) //Фильтр товаров по наименованию
-	router.GET("/NameDESC", Handler.NameDESC) //Фильтр товаров по наименованию в обратном порядке
-	router.GET("/PriceASC", Handler.PriceASC) //Фильтр товаров по цене
-	router.GET("/PriceDESC", Handler.PriceDESC) //Фильтр товаров по цене в обратном порядке
-	router.GET("/shop_single", Handler.Shop_single) //Просмотр карточки товара
+	router.GET("/NameASC", Handler.NameASC) //Фильтр товаров по наименованию ✅
+	router.GET("/NameDESC", Handler.NameDESC) //Фильтр товаров по наименованию в обратном порядке ✅
+	router.GET("/PriceASC", Handler.PriceASC) //Фильтр товаров по цене ✅
+	router.GET("/PriceDESC", Handler.PriceDESC) //Фильтр товаров по цене в обратном порядке ✅
+	router.GET("/shop_single", Handler.Shop_single) //Просмотр карточки товара ✅
 	router.GET("/SearhProduct", Handler.SearhProduct) //Поиск товара по ID или названию ✅
 	router.GET("/cart", Handler.Cart) //Корзина
 	router.GET("/about", Handler.About) //О нас ✅
